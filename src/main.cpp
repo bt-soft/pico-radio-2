d0032efe5db3e3008eb88b687a847fd56706c82c
bt-soft <bt-soft@freemail.hu>
2025-04-17 13:26:42 +0200

    Add initial project structure with configuration files, libraries, and Rotary Encoder implementation

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+
+//------------------- Rotary Encoder
+// Rotary Encoder
+#define PIN_ENCODER_CLK 17
+#define PIN_ENCODER_DT 16
+#define PIN_ENCODER_SW 18
+
+#include "RotaryEncoder.h"
+RotaryEncoder rotaryEncoder = RotaryEncoder(PIN_ENCODER_CLK, PIN_ENCODER_DT, PIN_ENCODER_SW, ROTARY_ENCODER_STEPS_PER_NOTCH);
+#define ROTARY_ENCODER_SERVICE_INTERVAL_IN_MSEC 1  // 1msec
+
+#define __USE_ROTARY_ENCODER_IN_HW_TIMER
+#ifdef __USE_ROTARY_ENCODER_IN_HW_TIMER
+// Pico Hardware timer a Rotary encoder olvasására
+#include <RPi_Pico_TimerInterrupt.h>
+RPI_PICO_Timer ITimer1(1);
+#endif
+
+#ifdef __USE_ROTARY_ENCODER_IN_HW_TIMER
+/**
+ * Hardware timer interrupt service routine a rotaryhoz
+ */
+bool hardwareTimerHandler1(struct repeating_timer *t) {
+    rotaryEncoder.service();
+    return true;
+}
+#endif
+
+//------------------- Rotary Encoder
+
+/**
+ * setup() - a program kezdőpontja
+ */
+void setup() {
+#ifdef __DEBUG
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+#endif
+
+    // Rotary Encoder beállítása
+    rotaryEncoder.setDoubleClickEnabled(true);
+    rotaryEncoder.setAccelerationEnabled(true);
+#ifdef __USE_ROTARY_ENCODER_IN_HW_TIMER
+    // Pico HW Timer1 beállítása a rotaryhoz
+    ITimer1.attachInterruptInterval(ROTARY_ENCODER_SERVICE_INTERVAL_IN_MSEC * 1000, hardwareTimerHandler1);
+#endif
+}
+
+/**
+ * loop() - a program fő ciklusa
+ */
+void loop() {
+    // put your main code here, to run repeatedly:
+}
