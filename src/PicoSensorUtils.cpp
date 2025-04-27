213c03dd9accd4ea563e5508fab3f35d3a6f3054
bt-soft <bt-soft@freemail.hu>
2025-04-27 20:47:15 +0200

    Refactor VBUS measurement constants: change R1, R2, and DIVIDER_RATIO from const float to #define for improved clarity and consistency in PicoSensorUtils.

--- src/PicoSensorUtils.cpp
+++ src/PicoSensorUtils.cpp
@@ -8,9 +8,9 @@ namespace PicoSensorUtils {
 
 // Külső feszültségosztó ellenállásai a VBUS méréshez (A0-ra kötve)
 #define PIN_VBUS_DIVIDER A0
-const float R1 = 197.5;                      // Ellenállás VBUS és A0 között (kOhm)
-const float R2 = 99.5;                       // Ellenállás A0 és GND között (kOhm)
-const float DIVIDER_RATIO = (R1 + R2) / R2;  // Feszültségosztó aránya
+#define R1 197.5f                       // Ellenállás VBUS és A0 között (kOhm)
+#define R2 99.5f                        // Ellenállás A0 és GND között (kOhm)
+#define DIVIDER_RATIO ((R1 + R2) / R2)  // Feszültségosztó aránya
 
 /**
  * ADC olvasás és VBUS feszültség kiszámítása külső osztóval
