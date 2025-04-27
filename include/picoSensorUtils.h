afc5fa5b28705d4a18c3353ff76a48e2507cb165
bt-soft <bt-soft@freemail.hu>
2025-04-27 21:02:51 +0200

    Refactor PicoSensorUtils: inline readVBus and readCoreTemperature functions, consolidate ADC voltage measurement logic, and remove redundant source file for improved clarity and modularity.

--- include/picoSensorUtils.h
+++ include/picoSensorUtils.h
@@ -5,7 +5,16 @@
 
 namespace PicoSensorUtils {
 
+// --- Konstansok ---
 #define AD_RESOLUTION 12  // 12 bites az ADC
+#define V_REFERENCE 3.3f
+#define CONVERSION_FACTOR (1 << AD_RESOLUTION)
+
+// Külső feszültségosztó ellenállásai a VBUS méréshez (A0-ra kötve)
+#define PIN_VBUS_DIVIDER A0
+#define R1 197.5f                       // Ellenállás VBUS és A0 között (kOhm)
+#define R2 99.5f                        // Ellenállás A0 és GND között (kOhm)
+#define DIVIDER_RATIO ((R1 + R2) / R2)  // Feszültségosztó aránya
 
 /**
  * AD inicializálása
@@ -13,16 +22,23 @@ namespace PicoSensorUtils {
 inline void init() { analogReadResolution(AD_RESOLUTION); }
 
 /**
- * ADC olvasás és VBUS feszültség kiszámítása külső osztóval GP29-en.
+ * ADC olvasás és VBUS feszültség kiszámítása külső osztóval
  * @return A VBUS mért feszültsége Voltban.
  */
-float readVBus();
+inline float readVBus() {
+
+    // ADC érték átalakítása feszültséggé
+    float voltageOut = (analogRead(PIN_VBUS_DIVIDER) * V_REFERENCE) / CONVERSION_FACTOR;
+
+    // Eredeti feszültség számítása a feszültségosztó alapján
+    return voltageOut * DIVIDER_RATIO;
+}
 
 /**
  * Kiolvassa a processzor hőmérsékletét
  * @return processzor hőmérséklete Celsius fokban
  */
-float readCoreTemperature();
+inline float readCoreTemperature() { return analogReadTemp(); }
 
 };  // namespace PicoSensorUtils
 
